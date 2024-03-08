1cb0472ab6a001d60524c714bd6a9cd9e35b55f3
Nikita Kartashev <nikitakartashev1@gmail.com>
2024-03-08 11:59:45 +0300

    trying check first task execution

---
+++ tasks.py
@@ -0,0 +1,14 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from db import tables, engine
+
+def first():
+    sql_query = select(tables['members'].c['surname']).distinct()
+    sql_query = sql_query.where(tables['members'].c.memid != 0)
+    sql_query = sql_query.order_by(tables['members'].c['surname']).limit(10)
+
+    with Session(engine) as session:
+        result = session.execute(sql_query).fetchall()
+
+    return result
