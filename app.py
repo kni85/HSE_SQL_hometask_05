923176763940a34af4034af3a236944f0701b80d
Nikita Kartashev <nikitakartashev1@gmail.com>
2024-03-09 21:38:19 +0300

    move functions in app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
-import tasks
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from db import tables, engine
 
 app = FastAPI()
-
 @app.get("/first_task")
-def first_task():
-    resp = tasks.first()
-    return resp
+def first() -> list:
+    sql_query = select(tables['members'].c['surname']).distinct()
+    sql_query = sql_query.where(tables['members'].c.memid != 0)
+    sql_query = sql_query.order_by(tables['members'].c['surname']).limit(10)
+    with Session(engine) as session:
+        result = session.execute(sql_query).fetchall()
+    result = [r[0] for r in result]
+    return result
 
+if __name__ == '__main__':
+    resp = first()
+    print(resp)
