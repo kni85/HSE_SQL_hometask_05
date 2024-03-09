65cf07835c94736bd8e5c891ead9b774601c8fa0
Nikita Kartashev <nikitakartashev1@gmail.com>
2024-03-09 17:07:23 +0300

    check first task query

--- app.py
+++ app.py
@@ -1,42 +1,10 @@
-# from typing import List
-from fastapi import FastAPI #, Depends
-# from sqlalchemy.orm import Session
-# from typing import Any
-# from pydantic import BaseModel
-
-# class FirstItem(BaseModel):
-#     name: str
-
-from tasks import first
-
-# resp = first()
-# print(resp)
+from fastapi import FastAPI
+import tasks
 
 app = FastAPI()
 
 @app.get("/first_task")
-def f():
-    resp = first()
+def first_task():
+    resp = tasks.first()
     return resp
 
-
-#
-#
-# def get_session():
-#     with SessionLocal() as session:
-#         return session
-#
-#
-# @app.get("/user/all", response_model=List[UserGet])
-# def get_all_users(limit: int = 10, db: Session = Depends(get_session)):
-#     return db.query(Member).limit(limit).all()
-#
-#
-# @app.get("/facility/all")
-# def get_all_facilities(limit: int = 1, db: Session = Depends(get_session)):
-#     return db.query(Facility).limit(limit).all()
-#
-#
-# @app.get("/booking/all", response_model=List[BookingGet])
-# def get_all_bookings(limit: int = 10, db: Session = Depends(get_session)):
-#     return db.query(Booking).limit(limit).all()
