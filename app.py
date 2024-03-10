505313231d8e0f7fdf858ab81e74534f3a6618b3
Nikita Kartashev <nikitakartashev1@gmail.com>
2024-03-10 20:06:15 +0300

    updated all api output using pydantic describe BaseModel in models.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import models
 
 app = FastAPI()
 
-@app.get("/first_task")
+@app.get("/first_task", response_model=List[models.first])
 def first_task() -> list:
     sql_query = (select(tables['members'].c['surname'])
                  .distinct()
@@ -19,11 +19,10 @@ def first_task() -> list:
                  .limit(10))
     with Session(engine) as session:
         result = session.execute(sql_query).fetchall()
-    result = [r[0] for r in result]
     return result
 
-@app.get("/second_task")
-def second_task() -> list:
+@app.get("/second_task", response_model=List[models.second])
+def second_task():
     sql_query = (select(tables['members'].c.firstname,
                         tables['members'].c.surname,
                         tables['members'].c.joindate)
@@ -33,16 +32,12 @@ def second_task() -> list:
     with Session(engine) as session:
         result = session.execute(sql_query).fetchall()
 
-    result = [{'firstname':  r[0],
-               'surname': r[1],
-               'joindate': r[2]}
-              for r in result]
     return result
 
-@app.get("/third_task")
-def third_task() -> list:
+@app.get("/third_task", response_model=List[models.third])
+def third_task():
     sql_query = (select(tables['bookings'].c.starttime,
-                        tables['facilities'].c.name)
+                        tables['facilities'].c.name.label('facility_name'))
                  .join(tables['facilities'])
                  .where(and_(tables['bookings'].c.starttime >= '2012-09-21',
                              tables['bookings'].c.starttime < '2012-09-22'))
@@ -51,17 +46,17 @@ def third_task() -> list:
     with Session(engine) as session:
         result = session.execute(sql_query).fetchall()
 
-    result = [{'start time':  r[0],
-               'facility name': r[1]}
-              for r in result]
     return result
 
-@app.get("/fourth_task")
-def fourth_task() -> list:
+@app.get("/fourth_task", response_model=List[models.fourth])
+def fourth_task():
     m = tables['members'].alias()
     mm = tables['members'].alias()
 
-    sql_query = (select(m.c.firstname, m.c.surname, mm.c.firstname, mm.c.surname)
+    sql_query = (select(m.c.firstname,
+                        m.c.surname,
+                        mm.c.firstname.label('recomendedby_name'),
+                        mm.c.surname.label('recomendedby_surname'))
                  .join(mm, m.c.recommendedby == mm.c.memid, isouter=True)
                  .where(m.c.memid != 0)
                  .order_by(m.c.firstname, m.c.surname))
@@ -69,28 +64,20 @@ def fourth_task() -> list:
     with Session(engine) as session:
         result = session.execute(sql_query).fetchall()
 
-    result = [{'name':  r[0],
-               'surname': r[1],
-               'rec_by_name': r[2],
-               'rec_by_surname': r[3]}
-              for r in result]
     return result
 
-@app.get("/fifth_task")
-def fifth_task() -> list:
+@app.get("/fifth_task", response_model=List[models.fifth])
+def fifth_task():
     b = tables['bookings'].alias()
-    sql_query = (select(b.c.facid, func.sum(b.c.slots).label('total_sum'))
+    sql_query = (select(b.c.facid, func.sum(b.c.slots).label('Total_slots'))
                  .where(and_(b.c.starttime >= '2012-09-01',
                              b.c.starttime < '2012-10-01'))
                  .group_by(b.c.facid)
-                 .order_by('total_sum'))
+                 .order_by('Total_slots'))
 
     with Session(engine) as session:
         result = session.execute(sql_query).fetchall()
 
-    result = [{'facid':  r[0],
-               'Total slots': r[1]}
-              for r in result]
     return result
 
 @app.get("/sixth_task", response_model=List[models.sixth])
@@ -158,4 +145,3 @@ if __name__ == '__main__':
     resp = sixth_task()
     print('Sixth task responds with the following:')
     print(resp)
-
--- models.py
+++ models.py
@@ -1,4 +1,27 @@
 from pydantic import BaseModel
+from datetime import datetime
+
+class first(BaseModel):
+    surname: str | None = None
+
+class second(BaseModel):
+    firstname: str | None = None
+    surname: str | None = None
+    joindate: datetime | None = None
+
+class third(BaseModel):
+    starttime: datetime | None = None
+    facility_name: str | None = None
+
+class fourth(BaseModel):
+    firstname: str | None = None
+    surname: str | None = None
+    recomendedby_name: str | None = None
+    recomendedby_surname: str | None = None
+
+class fifth(BaseModel):
+    facid: int | None = None
+    Total_slots: int | None = None
 
 class sixth(BaseModel):
     member: str | None = None
